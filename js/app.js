68466144ecfd9dfa4871878400784d192069c826
Mansoor-Kalash <mansoor.jow@gmail.com>
2021-06-27 13:11:02 +0300

    add list type

--- js/app.js
+++ js/app.js
@@ -6,21 +6,21 @@ function one() {
   let a1 = prompt(q1 + ' Should answer with Yes or No');
 
   switch (a1.toLowerCase()) {
-    case 'yes':
-    case 'y':
-      console.log('correct ans');
-      alert('correct ans ');
-      break;
+  case 'yes':
+  case 'y':
+    console.log('correct ans');
+    alert('correct ans ');
+    break;
 
-    case 'no':
-    case 'n':
-      console.log('uncorrect ans');
-      alert('uncorrect anss');
-      break;
-    default:
-      console.log('You misspelled it, but it\'s okay');
-      alert('You misspelled it, but it\'s okay');
-      break;
+  case 'no':
+  case 'n':
+    console.log('uncorrect ans');
+    alert('uncorrect anss');
+    break;
+  default:
+    console.log('You misspelled it, but it\'s okay');
+    alert('You misspelled it, but it\'s okay');
+    break;
   }
 
 }
@@ -32,22 +32,22 @@ function two() {
   let a2 = prompt(q2 + ' Should answer with Yes or No');
 
   switch (a2.toLowerCase()) {
-    case 'yes':
-    case 'y':
-      console.log('correct ans');
-      alert('correct ans');
-      break;
+  case 'yes':
+  case 'y':
+    console.log('correct ans');
+    alert('correct ans');
+    break;
 
-    case 'no':
-    case 'n':
-      console.log('uncorrect ans');
-      alert('uncorrect ans');
-      break;
+  case 'no':
+  case 'n':
+    console.log('uncorrect ans');
+    alert('uncorrect ans');
+    break;
 
-    default:
-      console.log('You misspelled it, but it\'s okay');
-      alert('You misspelled it, but it\'s okay');
-      break;
+  default:
+    console.log('You misspelled it, but it\'s okay');
+    alert('You misspelled it, but it\'s okay');
+    break;
   }
 }
 
@@ -59,21 +59,21 @@ function three() {
   let a3 = prompt(q3 + ' Should answer with Yes or No');
 
   switch (a3.toLowerCase()) {
-    case 'yes':
-    case 'y':
-      console.log('correct ans');
-      alert('correct ans');
-      break;
+  case 'yes':
+  case 'y':
+    console.log('correct ans');
+    alert('correct ans');
+    break;
 
-    case 'no':
-    case 'n':
-      console.log('uncorrect ans');
-      alert('uncorrect ans');
-      break;
-    default:
-      console.log('You misspelled it, but it\'s okay');
-      alert('You misspelled it, but it\'s okay');
-      break;
+  case 'no':
+  case 'n':
+    console.log('uncorrect ans');
+    alert('uncorrect ans');
+    break;
+  default:
+    console.log('You misspelled it, but it\'s okay');
+    alert('You misspelled it, but it\'s okay');
+    break;
   }
 }
 
@@ -85,24 +85,24 @@ function four() {
   let a4 = prompt(q4 + ' Should answer with Yes or No');
 
   switch (a4.toLowerCase()) {
-    case 'yes':
-    case 'y':
-      console.log('correct ans');
-      alert('correct ans');
-      break;
+  case 'yes':
+  case 'y':
+    console.log('correct ans');
+    alert('correct ans');
+    break;
 
-    case 'no':
-    case 'n':
+  case 'no':
+  case 'n':
 
-      console.log('uncorrect ans');
-      alert('uncorrect ans');
-      break;
+    console.log('uncorrect ans');
+    alert('uncorrect ans');
+    break;
 
-    default:
-      console.log('You misspelled it, but it\'s okay');
+  default:
+    console.log('You misspelled it, but it\'s okay');
 
-      alert('You misspelled it, but it\'s okay');
-      break;
+    alert('You misspelled it, but it\'s okay');
+    break;
   }
 }
 
@@ -112,148 +112,141 @@ function five() {
 
   let a5 = prompt(q5 + ' Should answer with Yes or No');
   switch (a5.toLowerCase()) {
-    case 'yes':
-    case 'y':
-      console.log('correct ans');
-      alert('correct ans');
-      break;
-    case 'no':
-    case 'n':
-      console.log('uncorrect ans');
-      alert('uncorrect ans');
-      break;
+  case 'yes':
+  case 'y':
+    console.log('correct ans');
+    alert('correct ans');
+    break;
+  case 'no':
+  case 'n':
+    console.log('uncorrect ans');
+    alert('uncorrect ans');
+    break;
 
-    default:
-      console.log('You misspelled it, but it\'s okay');
-      alert('You misspelled it, but it\'s okay');
-      break;
+  default:
+    console.log('You misspelled it, but it\'s okay');
+    alert('You misspelled it, but it\'s okay');
+    break;
   }
 }
 
-  function six() {
-    alert('guessing game: you will guess the Lucky number and it is between 0 -10 and you have 4 chances to guess the number and we will tell you if your guess is too high or too low, yoy are ready');
-    let lucky = 7; //Math.floor(Math.random() * 11);
-    let guess = Number(prompt('Guess the number?'));
+function six() {
+  alert('guessing game: you will guess the Lucky number and it is between 0 -10 and you have 4 chances to guess the number and we will tell you if your guess is too high or too low, yoy are ready');
+  let lucky = 7;
+  let guess = Number(prompt('Guess the number?'));
 
-    for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 3; i++) {
 
-      if ((guess < 0) || (guess > 10)) {
-        guess = Number(prompt('your guess is out of the range'));
+    if ((guess < 0) || (guess > 10)) {
+      guess = Number(prompt('your guess is out of the range'));
 
-      }
+    }
 
-      if (guess === lucky) {
-        alert('Correct answer ' + guess + ' is the correct number');
-        break;
-      }
-
-
-      if (Math.abs(lucky - guess) <= 3) {
-
-        guess = Number(prompt('Too high'));
+    if (guess === lucky) {
+      alert('Correct answer ' + guess + ' is the correct number');
+      break;
+    }
 
 
+    if (Math.abs(lucky - guess) <= 3) {
 
-      }
-
-
-
-      if (Math.abs(lucky - guess) >= 4) {
-
-        guess = Number(prompt('Too low'));
-
-
-      }
-
-
-
-
-
-      if (i === 2 && guess === lucky) {
-        alert('good luck the correct number is  ' + lucky);
-
-
-      }
-
-
-      if (i === 2 && guess !== lucky) {
-        alert('Hard luck the correct number is  ' + lucky);
-      }
-
+      guess = Number(prompt('Too high'));
 
 
 
     }
+
+
+
+    if (Math.abs(lucky - guess) >= 4) {
+
+      guess = Number(prompt('Too low'));
+
+
+    }
+
+
+
+
+
+    if (i === 2 && guess === lucky) {
+      alert('good luck the correct number is  ' + lucky);
+
+
+    }
+
+
+    if (i === 2 && guess !== lucky) {
+      alert('Hard luck the correct number is  ' + lucky);
+    }
+
+
+
 
   }
 
-
-
-
-
-  function seven() {
-    alert('guessing game: you will guess my multiple Luvky number and it is between 0 -10 and you have 6 chances to guess the number and we will tell you if your guess is too high or too low, yoy are ready');
-    let poss = [1, 3, 7];
-    //let cc;
-    // for (let i=0; i<3 ; i++)
-    // {
-    //   cc= Math.floor(Math.random() * 11);
-    //   poss.push(cc);
-    // }
-    let guessNum = [];
-    let score = 0;
-    let guess2 = Number(prompt('First chance?'));
-    console.log(guess2);
-    guessNum.push(guess2);
-    for (let i = 2; i < 7; i++) {
-      if (guess2 === poss[0] || guess2 === poss[1] || guess2 === poss[2]) {
-        // // alert ('Correct answer '+ guess + ' is the correct number');
-        // guess = Number(prompt('Guess the number'));
-        // guessNum.push(guess);
-        score = score + 1;
-      }
-
-      if ((guess2 >= 0) || (guess2 <= 10)) {
-        guess2 = Number(prompt('Chance Number ' + i));
-        guessNum.push(guess2);
-        console.log(guess2);
-      }
-
-      if ((guess2 < 0) || (guess2 > 10)) {
-        guess2 = Number(prompt('your guess is out of the range, Chance Number ' + i));
-        guessNum.push(guess2);
-        console.log(guess2);
-      }
+}
 
 
 
 
 
+function seven() {
+  alert('guessing game: you will guess my multiple Luvky number and it is between 0 -10 and you have 6 chances to guess the number and we will tell you if your guess is too high or too low, yoy are ready');
+  let poss = [1, 3, 7];
 
-    }
-
+  let guessNum = [];
+  let score = 0;
+  let guess2 = Number(prompt('First chance?'));
+  console.log(guess2);
+  guessNum.push(guess2);
+  for (let i = 2; i < 7; i++) {
     if (guess2 === poss[0] || guess2 === poss[1] || guess2 === poss[2]) {
+
       score = score + 1;
     }
 
+    if ((guess2 >= 0) || (guess2 <= 10)) {
+      guess2 = Number(prompt('Chance Number ' + i));
+      guessNum.push(guess2);
+      console.log(guess2);
+    }
 
-    console.log(guessNum);
-    alert('your Mark is ' + score + ' from 6');
-    alert('The correct Numbers ' + poss);
+    if ((guess2 < 0) || (guess2 > 10)) {
+      guess2 = Number(prompt('your guess is out of the range, Chance Number ' + i));
+      guessNum.push(guess2);
+      console.log(guess2);
+    }
 
 
 
-    let a = prompt('What is youre Name');
-    alert('Welcome ' + a + ', Youer name will finde in the bottom of the page');
-    document.write('<p style=\'hight: 50px ; background: #3b5998;color: white; \'> ' + a + '</p>');
+
+
 
   }
 
- one ();
- two () ;
+  if (guess2 === poss[0] || guess2 === poss[1] || guess2 === poss[2]) {
+    score = score + 1;
+  }
+
+
+  console.log(guessNum);
+  alert('your Mark is ' + score + ' from 6');
+  alert('The correct Numbers ' + poss);
+
+
+
+  let a = prompt('What is youre Name');
+  alert('Welcome ' + a + ', Youer name will finde in the bottom of the page');
+  document.write('<p style=\'hight: 50px ; background: #3b5998;color: white; \'> ' + a + '</p>');
+
+}
+
+one ();
+two () ;
 three () ;
 four () ;
- five() ;
- six();
+five() ;
+six();
 seven ();
 
